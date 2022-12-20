000878643b5d7bbecbceaa3a72447de9c0650bd0
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-12-20 12:36:51 +0100

    tercer cambio en submodulo

--- src/mylibrary.cpp
+++ src/mylibrary.cpp
@@ -4,4 +4,5 @@ void funcion_libreria()
 	std::cout << "funcion_libreria" << std::endl;
 	std::cout << "cambio por descargar en el proyecto principal" << std::endl;
 	std::cout << "segundo cambio por descargar en proyecto principal" << std::endl;
+	std::cout << "tercer cambio por descargar en el tag que se ha creado en el principal" << std::endl;
 }
