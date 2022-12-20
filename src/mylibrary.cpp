662593f1e48f082145ce526f2ddfbb15da31fd65
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-12-20 11:08:16 +0100

    segundo cambio en submodulo

--- src/mylibrary.cpp
+++ src/mylibrary.cpp
@@ -3,5 +3,5 @@ void funcion_libreria()
 {
 	std::cout << "funcion_libreria" << std::endl;
 	std::cout << "cambio por descargar en el proyecto principal" << std::endl;
-	std::cout << "segundo cambio por descargar en el proyecto principal" << std::endl;
+	std::cout << "segundo cambio por descargar en proyecto principal" << std::endl;
 }
