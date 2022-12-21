54e862c01edcf60c362f82d1a489c976a02e1d9d
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-12-21 08:50:53 +0100

    cuarto cambio que se ha hecho en el principal

--- src/mylibrary.cpp
+++ src/mylibrary.cpp
@@ -5,4 +5,5 @@ void funcion_libreria()
 	std::cout << "cambio por descargar en el proyecto principal" << std::endl;
 	std::cout << "segundo cambio por descargar en proyecto principal" << std::endl;
 	std::cout << "tercer cambio por descargar en el tag que se ha creado en el principal" << std::endl;
+	std::cout << "cuarto cambio que se ha hecho en el principal" << std::endl;
 }
