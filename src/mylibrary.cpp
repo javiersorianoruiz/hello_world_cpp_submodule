712ca3ecf1eeb41c57f151c88861591663ad5462
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2023-01-10 13:55:19 +0100

    main limpio rama Feature_EVC_MonoChannel

--- src/mylibrary.cpp
+++ src/mylibrary.cpp
@@ -1,10 +1,5 @@
 
 void funcion_libreria()
 {
-	std::cout << "funcion_libreria" << std::endl;
-	std::cout << "cambio por descargar en el proyecto principal" << std::endl;
-	std::cout << "segundo cambio por descargar en proyecto principal" << std::endl;
-	std::cout << "tercer cambio por descargar en el tag que se ha creado en el principal" << std::endl;
-	std::cout << "cuarto cambio que se ha hecho en el principal" << std::endl;
-	std::cout << "quinto cambio hecho en el principal pero en la nueva rama del submodulo" << std::endl;
+	std::cout << "funcion_libreria primer cambio rama Feature_EVC_MonoChannel" << std::endl;
 }
