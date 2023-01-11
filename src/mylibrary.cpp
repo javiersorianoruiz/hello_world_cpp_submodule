80114332a34c035fdb27a7c98db79411a5512615
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2023-01-11 17:23:54 +0100

    cambio 1 en Feature_EVC_MonoChannel

--- src/mylibrary.cpp
+++ src/mylibrary.cpp
@@ -2,4 +2,6 @@
 void funcion_libreria()
 {
 	std::cout << "funcion_libreria primer cambio rama Feature_EVC_MonoChannel" << std::endl;
+
+	std::cout << "cambio tras ckeckout Feature_EVC_MonoChannel" << std::endl;
 }
